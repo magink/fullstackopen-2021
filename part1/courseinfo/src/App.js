018433a3a3681c95088808450be9a22339f5482f
magink <magink1991@gmail.com>
2021-03-13 18:19:48 +0100

    exercise 1.3 done

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -7,12 +7,12 @@ const Header = ({ course }) => {
     </>
   );
 };
-const Content = ({ parts, exercises }) => {
+const Content = ({ parts }) => {
   return (
     <>
-      <Part part={parts[0]} exercises={exercises[0]}></Part>
-      <Part part={parts[1]} exercises={exercises[1]}></Part>
-      <Part part={parts[2]} exercises={exercises[2]}></Part>
+      <Part part={parts[0].name} exercises={parts[0].exercises}></Part>
+      <Part part={parts[1].name} exercises={parts[1].exercises}></Part>
+      <Part part={parts[2].name} exercises={parts[2].exercises}></Part>
     </>
   );
 };
@@ -36,20 +36,23 @@ const Total = ({ total }) => {
 
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
+  const part1 = {
+    name: "Fundamentals of React",
+    exercises: 10,
+  };
+  const part2 = {
+    name: "Using props to pass data",
+    exercises: 7,
+  };
+  const part3 = {
+    name: "State of a component",
+    exercises: 14,
+  };
   return (
     <div>
       <Header course={course} />
-      <Content
-        parts={[part1, part2, part3]}
-        exercises={[exercises1, exercises2, exercises3]}
-      />
-      <Total total={exercises1 + exercises2 + exercises3} />
+      <Content parts={[part1, part2, part3]} />
+      <Total total={part1.exercises + part2.exercises + part3.exercises} />
     </div>
   );
 };
