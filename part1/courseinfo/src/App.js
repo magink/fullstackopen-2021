31decdaa4f8c5c4db92d0fe708fd7850c7408e43
magink <magink1991@gmail.com>
2021-03-13 17:57:47 +0100

    exercise 1.2 done

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -7,7 +7,16 @@ const Header = ({ course }) => {
     </>
   );
 };
-const Content = ({ part, exercises }) => {
+const Content = ({ parts, exercises }) => {
+  return (
+    <>
+      <Part part={parts[0]} exercises={exercises[0]}></Part>
+      <Part part={parts[1]} exercises={exercises[1]}></Part>
+      <Part part={parts[2]} exercises={exercises[2]}></Part>
+    </>
+  );
+};
+const Part = ({ part, exercises }) => {
   return (
     <>
       <p>
@@ -36,9 +45,10 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} exercises={exercises1} />
-      <Content part={part2} exercises={exercises2} />
-      <Content part={part3} exercises={exercises3} />
+      <Content
+        parts={[part1, part2, part3]}
+        exercises={[exercises1, exercises2, exercises3]}
+      />
       <Total total={exercises1 + exercises2 + exercises3} />
     </div>
   );
