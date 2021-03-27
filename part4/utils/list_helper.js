6a643e1122157551bb3b98de5ce3a21e426560b1
magink <magink1991@gmail.com>
2021-03-27 20:02:45 +0100

    test: tests for list_helper functions

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,7 +1,17 @@
 const dummy = (blogs) => {
-  // ...
+  return 1;
+};
+const totalLikes = (blogList) => {
+  const reducer = (acc, curr) => acc + curr.likes;
+  return blogList.reduce(reducer, 0);
+};
+const favoriteBlog = (blogList) => {
+  const reducer = (fav, curr) => fav.likes > curr.likes ? fav : curr;
+  return blogList.reduce(reducer, 0);
 };
 
 module.exports = {
-  dummy
+  dummy,
+  totalLikes,
+  favoriteBlog
 };
