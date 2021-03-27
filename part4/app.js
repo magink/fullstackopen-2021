ee974a5df46d7ccf4072580905435ccd14e897e7
magink <magink1991@gmail.com>
2021-03-27 16:06:59 +0100

    refactor: mongoose model to module

--- part4/app.js
+++ part4/app.js
@@ -3,15 +3,9 @@ const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
 const config = require('./utils/config');
+const Blog = require('./models/blog');
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-});
-
-const Blog = mongoose.model('Blog', blogSchema);
+console.log(Blog);
 
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true });
 
