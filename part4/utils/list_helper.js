7f4c5e01d19e21de8ae51291c50b35b9f4757abf
magink <magink1991@gmail.com>
2021-03-28 13:16:45 +0200

    exercise 4.6* done

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const dummy = (blogs) => {
   return 1;
 };
@@ -9,9 +10,26 @@ const favoriteBlog = (blogList) => {
   const reducer = (fav, curr) => fav.likes > curr.likes ? fav : curr;
   return blogList.reduce(reducer, 0);
 };
+/*
+1. Count number of occurrences with countBy()
+2. Map author with most occurrences
+3. Return new object with author and number of blogs
+
+Returns the author who has the largest amount of blogs looking like this
+{
+  author: "Robert C. Martin",
+  blogs: 3
+}
+*/
+const mostBlogs = (blogList) => {
+  return _.chain(blogList)
+    .countBy('author')
+    .map((blogs, author) => ({ author, blogs }))
+    .maxBy('blogs')
+    .value();
+};
+
 
 module.exports = {
-  dummy,
-  totalLikes,
-  favoriteBlog
+  dummy, totalLikes, favoriteBlog, mostBlogs
 };
